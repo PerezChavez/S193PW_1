e940a173960b4f3997cc37999325475e946bd9ac
Carol Chavez <122042076@upq.edu.mx>
2024-10-17 15:50:25 -0600

    Paso 3 Creación de la portada

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+
+        .full-height {
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .content-box {
+            padding: 20px;
+            border: 2px solid #bd4691;
+            border-radius: 10px;
+            background-color: white;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 2.5rem;
+            color: #f41da2;
+        }
+
+        h2 {
+            font-size: 2rem;
+            color: #e626af;
+        }
+
+        h3 {
+            font-size: 1.5rem;
+            color: #8e44ad;
+        }
+    </style>
+</head>
+<body>
+
+<div class="full-height">
+    <div class="content-box">
+        <h1>Carol Paola Chavez Piña</h1>
+        <h2>Programación Web</h2>
+        <h3>S193 - Repaso Laravel</h3>
+    </div>
+</div>
+
+</body>
+</html>
--- practicaRepasos/routes/web.php
+++ practicaRepasos/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ Route::get('/', function () {
+   return view('inicio');
+ });
+
+
+//Route::view('/','Inicio')->name('rutaInicio');
