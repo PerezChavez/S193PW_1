bfd6bca184bd2918e67a725483fc5781fad0a25f
Carol Chavez <122042076@upq.edu.mx>
2024-10-30 23:45:11 -0600

    Creación de inicio

--- RepasoII/routes/web.php
+++ RepasoII/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
+});
+
+Route::get('/form', function(){
+    return view('FormularioLibro');
 });
